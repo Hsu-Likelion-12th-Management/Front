583d9e10168a0c9f724c0e93ac031a672dd4c32a
이나경 <lnk5324@hansung.ac.kr>
2024-02-04 13:28:49 +0900

    24-02-04

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 import { GlobalStyle } from './style/globalStyle';
 import Main from './main/main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './header/header';
+import Activity from './activity/activity';
 
 const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  max-width: 393px;
-  width: 100vw;
-  min-height: 852px;
-  background-color: #000000;
+
+  max-width: 428px;
+  background-color: black;
+  position: relative;
 `;
 
 function App() {
@@ -18,8 +23,10 @@ function App() {
     <Router>
       <GlobalStyle />
       <Wrapper>
+        <Header />
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/Activity" element={<Activity />} />
         </Routes>
       </Wrapper>
     </Router>
