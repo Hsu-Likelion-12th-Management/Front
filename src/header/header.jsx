089f3a40397dd842a2e2dc2a1a1979190dcaed3e
이나경 <lnk5324@hansung.ac.kr>
2024-02-06 01:08:52 +0900

    Edit Activity page

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -7,6 +7,7 @@ const HeaderContainer = styled.div`
   padding-left: 24px;
   width: 100%;
   height: 90px;
+  max-width: 428px;
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
