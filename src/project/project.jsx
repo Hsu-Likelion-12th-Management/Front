95852064e28b7f139b45882e4e041e75b296a9d9
Jun279 <33.beautifulboy@gmail.com>
2024-02-06 17:48:35 +0900

    header 수정

--- src/project/project.jsx
+++ src/project/project.jsx
@@ -1,5 +1,17 @@
+import Intro from "../Intro/Intro";
+import hexagon from "../images/hexagon.svg";
+
 function Project() {
-  return <></>;
+  return (
+    <>
+      <Intro>활동 소개</Intro>
+      <div>
+        <div className="topImgContainer">
+          <img src={hexagon}/>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Project;
--- src/project/project11.jsx
+++ src/project/project11.jsx
@@ -1,36 +1,5 @@
-import styled from "styled-components";
-import Hsulogo from '../images/hsulogo.png';
-
-const IntroContainer = styled.div`
-  width: 100%;
-  height: 80px;
-  padding-top: 26px;
-  padding-bottom: 25px;
-  padding-left: 20px;
-  background: linear-gradient(to right, #ff7710, #191b24 60px);
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  text-align: center;
-`;
-
-const IntroP = styled.p`
-  font-family: Pretendard;
-  font-size: 24px;
-  font-weight: bold;
-  margin-left: 16px;
-  color: ${(props) => props.color || 'white'};
-`;
-
 function Project11() {
-  return (
-    <>
-      <IntroContainer>
-        <img src={Hsulogo} alt="한성로고" style={{ width: "8%" }} />
-        <IntroP>활동 소개</IntroP>
-      </IntroContainer>
-    </>
-  );
+  return <></>;
 }
 
 export default Project11;
