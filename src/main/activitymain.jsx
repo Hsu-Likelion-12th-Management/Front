9f34d35fde3d9350b6e92717c928a86c710f2c71
Sengoo <lesley023@naver.com>
2024-02-08 12:53:40 +0900

    feat : 메인페이지 완성(반응형x)
    미디어쿼리 추가 전

--- src/main/activitymain.jsx
+++ src/main/activitymain.jsx
@@ -1,4 +1,8 @@
 import ActivityDetail from "./activitydetail";
+import detail1 from '../images/detail1.jpg';
+import detail2 from '../images/detail2.jpg';
+import detail3 from '../images/detail3.jpg';
+import detail4 from '../images/detail4.png';
 
 function ActivityMain(){
 
@@ -18,15 +22,41 @@ function ActivityMain(){
         marginBottom: '80px'
     }
 
+    const activities = [
+        {
+            image: detail1,
+            title: "팀 별 스터디",
+            description: "기획, 디자인, 프론트엔드, 백엔드 4개의 팀으로",
+            description2: "주 1회 세션을 진행합니다."
+        },
+        {
+            image: detail2,
+            title: "아이디어톤",
+            description: "아이디어를 발제하고, 디자인하는 과정을 거쳐",
+            description2: "타 대학과 경쟁합니다."
+        },
+        {
+            image: detail3,
+            title: "무박해커톤",
+            description: "여름방학, 약 한 달간의 프로젝트 과정을 거쳐",
+            description2: "무박으로 개발을 마무리하는 해커톤입니다."
+        },
+        {
+            image: detail4,
+            title: "연합 해커톤",
+            description: "타 대학 멋사들과 팀빌딩이 되어",
+            description2: "프로젝트할 수 있는 해커톤입니다."
+        }
+        ];
+
     return(
         <>
         <div>
             <div style={titleStyle}>어떤 활동을 하나요?</div>
             <div style={comContainerStyle}>
-            <ActivityDetail/>
-            <ActivityDetail/>
-            <ActivityDetail/>
-            <ActivityDetail/>
+            {activities.map((activity, index) => (
+            <ActivityDetail key={index} image={activity.image} title={activity.title} description={activity.description} description2={activity.description2} />
+            ))}
             </div>
         </div>
         </>
