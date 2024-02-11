1d190b00bbe15510fdcd40273469561c625cc890
Jun279 <33.beautifulboy@gmail.com>
2024-02-11 16:06:30 +0900

    Identity Verification 1차 작업

---
+++ src/IdentityVerification/IdentityVerification.jsx
@@ -0,0 +1,20 @@
+import "./IdentityVerification.css";
+import close from "../images/close.svg";
+
+export default function IdentityVerification() {
+  return (
+    <div className="bgContainer">
+      <button>
+        <img src={close} alt="closeImg" className="closeImg" />
+      </button>
+      <p className="ivText">본인 확인</p>
+      <div className="inputContainer">
+        <input type="text" placeholder="이름"/>
+        <input type="password" placeholder="비밀번호"/>
+      </div>
+      <button className="confirmButton">
+        확인
+      </button>
+    </div>
+  )
+}
