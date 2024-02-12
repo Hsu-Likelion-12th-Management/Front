3c0b454e387a2f67c3ebbd6a4502700fccdba05d
Jun279 <33.beautifulboy@gmail.com>
2024-02-12 14:00:33 +0900

    project 페이지 ui 완성

--- src/projectComponents/ImgBanner.jsx
+++ src/projectComponents/ImgBanner.jsx
@@ -4,9 +4,12 @@ import rightButton from "../images/rightButton.svg";
 import life from "../images/life.png";
 import happiness from "../images/happiness.png";
 import moah from "../images/moah.png";
+import lisa from "../images/prj4.PNG";
+import book from "../images/prj5.PNG";
+import bear from "../images/prj6.PNG";
 
 export default function ImgBanner() {
-  const projectImg = [life, happiness, moah];
+  const projectImg = [life, happiness, moah, bear, book, lisa];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const moveRight = () => {
@@ -28,7 +31,7 @@ export default function ImgBanner() {
         </button>
       </div>
       <div className="bannerImg">
-        <img src={projectImg[currentIndex]} alt="life" />
+        <img src={projectImg[currentIndex]} alt="life" style={{ width: "20.75rem", height: "11.625rem" }}/>
       </div>
       <div className="buttonBox">
         <button className="rigthButton" onClick={moveRight}>
